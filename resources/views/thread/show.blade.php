c92bd3907ac0cc313f3bd439fe4b255c064bd7ef
R. Kukuh <rkukuh@gmail.com>
2018-03-06 21:31:44 +0700

    A Thread Can Have Replies

--- resources/views/thread/show.blade.php
+++ resources/views/thread/show.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    {{ $thread->title }}
+                    <h4>{{ $thread->title }}</h4>
                 </div>
 
                 <div class="panel-body">
@@ -14,6 +14,26 @@
                 </div>
             </div>
         </div>
+    </div>
+
+    <div class="row">
+        @foreach ($thread->replies as $reply)
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <a href="#">
+                            {{ $reply->owner->name }}
+                        </a> said 
+
+                        {{ $reply->created_at->diffForHumans() }}
+                    </div>
+
+                    <div class="panel-body">
+                        {{ $reply->body }}
+                    </div>
+                </div>
+            </div>
+        @endforeach
     </div>
 </div>
 @endsection
---
+++ tests/Feature/ReadThreadsTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Reply;
+use App\Thread;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ReadThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->thread = factory(Thread::class)->create();
+    }
+
+    /** @test */
+    public function a_user_can_view_all_threads()
+    {
+        $this->get('/thread')
+             ->assertSee($this->thread->title);
+    }
+
+    /** @test */
+    public function a_user_can_read_a_single_thread()
+    {
+        $this->get('/thread/' . $this->thread->id)
+             ->assertSee($this->thread->title);
+    }
+
+    /** @test */
+    public function a_user_can_read_replies_associated_with_a_thread()
+    {
+        $reply = factory(Reply::class)->create([
+            'thread_id' => $this->thread->id
+        ]);
+
+        $this->get('/thread/' . $this->thread->id)
+             ->assertSee($reply->body);
+    }
+}
